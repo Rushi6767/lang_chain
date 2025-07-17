c8814067974845972ba04b56b353564ee42b93cd
Rushi6767 <rushisathavara581998@gmail.com>
2025-07-17 14:37:00 -0400

    Document_loader in Langchain

---
+++ 7_document_loader/1_text_loader.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template='summery assignment, i make e-commerce with ml features - \n {assignment}',
+    input_variables=['assignment']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader('text_loader.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'assignment':docs[0].page_content}))
